a9a1ab2844ff856aa37e1651561243e51819f43c
KarlDN <karl.newman@ntlworld.com>
2025-10-02 20:06:12 +0100

    add alien speed and control settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -41,6 +41,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
             self.clock.tick(60)
 
@@ -88,20 +89,34 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+    def _update_aliens(self):
+        """Update the positions of all aliens in the fleet."""
+        self.aliens.update()
+
     def _create_fleet(self):
         """Create the fleet of aliens."""
         # Create an alien and keep adding aliens until there's no room left.
-        # Spacing between alien's is one alien width.
+        # Spacing between alien's is one alien width and one alien height.
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
 
-        current_x = alien_width
-        while current_x < (self.settings.screen_width - 2 * alien_width):
-            new_alien = Alien(self)
-            new_alien.x = current_x
-            new_alien.rect.x = current_x
-            self.aliens.add(new_alien)
-            current_x += 2 * alien_width
+        current_x, current_y = alien_width, alien_height
+        while current_y < (self.settings.screen_height - 3 * alien_height):
+            while current_x < (self.settings.screen_width - 2 * alien_width):
+                self._create_alien(current_x, current_y)
+                current_x += 2 * alien_width
+
+            # Finished a row; reset x value, and incrementr y value.
+            current_x = alien_width
+            current_y += 2 * alien_height
+
+    def _create_alien(self, X_position, y_position):
+        """Create an alien and place it in the fleet."""
+        new_alien = Alien(self)
+        new_alien.x = X_position
+        new_alien.rect.x = X_position
+        new_alien.rect.y = y_position
+        self.aliens.add(new_alien)
                 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
