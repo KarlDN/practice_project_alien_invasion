b42fe2b79970e710a4d5d5f5dba1f87b4eed2ae5
KarlDN <karl.newman@ntlworld.com>
2025-10-05 15:05:09 +0100

    Code completed

--- alien_invasion.py
+++ alien_invasion.py
@@ -84,6 +84,9 @@ class AlienInvasion:
             # Reset the game statistics.
             self.settings.initialise_dynamic_settings()
             self.stats.reset_stats()
+            self.sb.prep_score()
+            self.sb.prep_level()
+            self.sb.prep_ships()
             self.game_active = True
 
             # Get rid of any remaining bullets and aliens.
@@ -139,11 +142,22 @@ class AlienInvasion:
         #   If so, get rid of the bullet and the alien.
         collisions = pygame.sprite.groupcollide(
                 self.bullets, self.aliens, True, True)
+        
+        if collisions:
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
+            self.sb.prep_score()
+            self.sb.check_high_score()
+
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+
+            # Increase level.
+            self.stats.level += 1
+            self.sb.prep_level()
 
     def _update_aliens(self):
         """Check if the fleet is at an edge, then update position."""
@@ -198,8 +212,9 @@ class AlienInvasion:
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
         if self.stats.ships_left > 0:
-            # Decrement ships_left.
+            # Decrement ships_left, and update scoreboard.
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             # Get rid of any remaining bullets and aliens.
             self.bullets.empty()
@@ -220,7 +235,7 @@ class AlienInvasion:
         for alien in self.aliens.sprites():
             if alien.rect.bottom >= self.settings.screen_height:
                 # Treat this the same as if the ship got hit.
-                self.ship_hit()
+                self._ship_hit()
                 break
            
     def _update_screen(self):
--- settings.py
+++ settings.py
@@ -24,15 +24,28 @@ class Settings:
 
         # How quickly the game speeds up.
         self.speedup_scale = 1.1
+        # How quickly the alien point values increase.
+        self.score_scale = 1.5
 
         self.initialise_dynamic_settings()
 
     def initialise_dynamic_settings(self):
         """Initialise settings that change throughout the game."""
-        self.ship_speed = 1.7
+        self.ship_speed = 1.5
         self.bullet_speed = 2.5
         self.alien_speed = 1.0
 
+        # Scoring settings.
+        self.alien_points = 50
+
         # fleet_direction of 1 represents right; -1 represents left.
-        self.fleet_direction = 1
-        
+        self.fleet_direction = 1       
+
+    def increase_speed(self):
+        """Increase speed settings and alien point values."""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
+        # print(self.alien_points)
